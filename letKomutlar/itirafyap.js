4b86dc0835c0a3ed4abbd7d245e1956003559de7
MrSn <105975100+LetCodes@users.noreply.github.com>
2023-02-06 11:21:38 +0400

    Update itirafyap.js

--- letKomutlar/itirafyap.js
+++ letKomutlar/itirafyap.js
@@ -23,10 +23,11 @@ module.exports = {
         if (!itirafChannel) return;
       
         const channel = interaction.guild.channels.cache.get(itirafChannel);
+      //--------------İtiraf kanalına mesaj gönderme
+
         try{
           channel.send({
             embeds: [new MessageEmbed()
-              .setAuthor({name:interaction.user.tag,iconURL: interaction.user.avatarURL()})
               .setDescription(`Hey millet! Yeni bir itiraf geldi! \r\n ---------- \r\n Ama itirafın sahibinin kim olduğunu size söyleyemem.\r\n ---------- \r\n İşte gizli kişinin yaptığı itiraf; \r\n || ${itiraf} ||`)
               .setColor("#2ACAEA")
               .setFooter({text:`${interaction.guild.name}`})
@@ -34,10 +35,15 @@ module.exports = {
             ]
           })
         }
+         //--------------İtiraf kanalına mesaj gönderme
+
         catch{
       
+          
+          
+          
         }
-   //--------------İtiraf kanalına mesaj gönderme
+   //--------------İtiraf-admin kanalına mesaj gönderme
 const { itirafadminChannel } = await model.findOne({ GuildID: interaction.guild.id }) || { itirafadminChannel: null };
         if (!itirafadminChannel) return;
       
@@ -45,6 +51,7 @@ const { itirafadminChannel } = await model.findOne({ GuildID: interaction.guild.
         try{
           chanmnel.send({
             embeds: [new MessageEmbed()
+              .setAuthor({name:interaction.user.tag,iconURL: interaction.user.avatarURL()})   
               .setDescription(`Hey admin! Yeni bir itiraf geldi! \r\n ---------- \r\n İtirafın sahibi: ${uye}\r\n ---------- \r\n İşte  kişinin yaptığı itiraf; \r\n || ${itiraf} ||`)
               .setColor("#2ACAEA")
               .setFooter({text:`${interaction.guild.name}`})
@@ -52,6 +59,8 @@ const { itirafadminChannel } = await model.findOne({ GuildID: interaction.guild.
             ]
           })
         }
+      //--------------İtiraf-admin kanalına mesaj gönderme
+
         catch{
       
         }
