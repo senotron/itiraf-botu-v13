eedbaac33aa689eaa59d9fed2647f475b5f03722
senotron <105975100+senotron@users.noreply.github.com>
2025-01-04 11:33:43 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client, interaction) => {
-    console.log(`${client.user.tag} ismi ile giriş yapıldı`);
-    client.user.setPresence({activities: [{name:"LetCode",status:"dnd"}], });   
+    console.log(`${client.user.tag} online!`);
+    client.user.setPresence({activities: [{name:"senotron",status:"dnd"}], });   
 };
